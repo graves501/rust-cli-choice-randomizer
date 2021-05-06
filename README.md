adfa49baa99c38d25b3f7fdaa2f1a35e268f5e45
graves501 <graves501@protonmail.com>
2021-05-06 23:38:54 +0200

    init: basic implementation

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust Choice Randomizer
+
+Add your choices and let this simple application decide for you!
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use rand::seq::SliceRandom;
+use std::io;
+
+fn main() {
+    let mut choices: Vec<String> = Vec::new();
+
+    loop {
+        println!("\nAdd your choice to the list!");
+        let mut choice = String::new();
+
+        io::stdin()
+            .read_line(&mut choice)
+            .expect("Failed to read input");
+
+        let choice: String = match choice.trim().parse() {
+            Ok(choice) => choice,
+            Err(_) => continue,
+        };
+
+        if !choice.is_empty() {
+            choices.push(choice);
+        }
+
+        if !choices.is_empty() {
+            println!("\nYour choices: {:#?}", choices);
+        }
+
+        println!("\nDo you want to add another entry? y/n");
+
+        let mut reply = String::new();
+
+        io::stdin()
+            .read_line(&mut reply)
+            .expect("Failed to read input");
+
+        let reply = reply.trim().parse::<char>().unwrap_or(' ');
+
+        match reply {
+            'y' => continue,
+            'n' => break,
+            _ => println!("Invalid input! Type y or n!"),
+        };
+    }
+
+    let random_choice = choices
+        .choose(&mut rand::thread_rng())
+        .expect("Could not get random element from choices vec!");
+
+    println!("\n\nYour random choice is: {}", random_choice);
+}
