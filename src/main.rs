879a4af88443a882157ea8a66b827fb84bbd3143
graves501 <graves501@protonmail.com>
2021-12-19 00:01:53 +0100

    refactor: rename some variables, set user input to lowercase

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,42 @@ use rand::seq::SliceRandom;
 use std::io;
 
 fn main() {
-    let mut choices: Vec<String> = Vec::new();
+    let mut choice_list: Vec<String> = Vec::new();
 
     loop {
         println!("\nAdd your choice to the list!");
-        let mut choice = String::new();
+        let mut new_choice_input = String::new();
 
         io::stdin()
-            .read_line(&mut choice)
+            .read_line(&mut new_choice_input)
             .expect("Failed to read input");
 
-        let choice: String = match choice.trim().parse() {
+        let new_choice: String = match new_choice_input.trim().parse() {
             Ok(choice) => choice,
             Err(_) => continue,
         };
 
-        if !choice.is_empty() {
-            choices.push(choice);
+        if !new_choice.is_empty() {
+            choice_list.push(new_choice);
         }
 
-        if !choices.is_empty() {
-            println!("\nYour choices: {:#?}", choices);
+        if !choice_list.is_empty() {
+            println!("\nYour choices: {:#?}", choice_list);
         }
 
         println!("\nDo you want to add another entry? y/n");
 
-        let mut reply = String::new();
+        let mut user_reply = String::new();
 
         io::stdin()
-            .read_line(&mut reply)
+            .read_line(&mut user_reply)
             .expect("Failed to read input");
 
-        let parsed_reply = reply.trim().parse::<char>();
+        let parsed_user_reply = user_reply.trim().parse::<char>();
 
-        match parsed_reply {
+        match parsed_user_reply {
             Ok(reply) => {
+                let reply = reply.to_ascii_lowercase();
                 match reply {
                     'y' => continue,
                     'n' => break,
@@ -47,7 +48,7 @@ fn main() {
         }
     }
 
-    let random_choice = choices
+    let random_choice = choice_list
         .choose(&mut rand::thread_rng())
         .expect("Could not get random element from choices vec!");
 
